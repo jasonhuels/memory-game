9225c2818459b2df098182b0ab1fabb4db16190b
Student <example@example.com>
2019-09-10 09:11:17 -0700

    add random gif button

--- src/main.js
+++ src/main.js
@@ -4,3 +4,27 @@ import './styles.css';
 import $ from 'jquery';
 
 import {Tamagotchi} from './tamagotchi.js';
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    // const city = $('#location').val();
+    // $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/random?api_key=APITMcCcU4sQTAn3bgnInPVQz9f7pX8a`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+      $('#gif').attr('src', `${response.data.images.original.url}`);
+    }
+  });
+});
